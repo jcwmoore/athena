1fafc2fabe9c680445ae3da6db2e1e951f2f88cf
John Moore <jcwmoore@gmail.com>
2013-07-17 19:20:28 -0500

    added my clone of csharp sqlite

---
+++ Athena.SQLite.Entity/Client/SQLiteClientFactory.Entity.cs
@@ -0,0 +1,46 @@
+//
+// SQLiteClinetFactory.Entity.cs
+//
+// Author:
+//       John Moore <jcwmoore@gmail.com>
+//
+// Copyright (c) 2013 John Moore
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Data;
+using System.Data.Common;
+
+namespace System.Data.SQLite
+{
+	public partial class SQLiteClientFactory : IServiceProvider
+	{
+		#region IServiceProvider implementation
+		
+		public object GetService (Type serviceType)
+		{
+			return SQLiteProviderServices.Instance;
+		}
+		
+		#endregion
+
+	}
+}
+
--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Every good developer should have one of these, this is my personal library of he
 
 Ioc
 ======
-Why another IoC ontainer?  Well I didn't really care for any of the ones out there under the MIT license and so I rolled my own out of a need and a desire to better understand IOC.  Use it if you like, otherwise I don't really care.
+Why another IoC container?  Well I didn't really care for any of the ones out there under the MIT license and so I rolled my own out of a need and a desire to better understand IOC.  Use it if you like, otherwise I don't really care.
+
+SQLite
+======
+SQLite libraries are my personal clone of the csharp-sqlite (http://code.google.com/p/csharp-sqlite/) project.  I have added an Android build and added an entity framework provider for the library.  Portions of the Entity Framework library fall under the MS-PL and/or public domain and are copy written by the person/organization noted in the code files.
