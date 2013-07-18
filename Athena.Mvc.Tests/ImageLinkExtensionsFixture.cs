6b996f305035555d6f07c02f535c55d6509f7680
John Moore <jcwmoore@gmail.com>
2013-07-18 15:52:18 -0500

    added my mvc helpers

---
+++ Athena.Mvc.Tests/ImageLinkExtensionsFixture.cs
@@ -0,0 +1,76 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using NUnit.Framework;
+
+namespace Athena.Mvc.Tests
+{
+    [TestFixture]
+    public class ImageLinkExtensionsFixture
+    {
+        [Test]
+        public void ImageLinkTest()
+        {
+            var target = CreateHtmlHelper(new ViewDataDictionary());
+            var res = target.ImageLink("ActionName", "test://image", "alt");
+            Assert.That(res, Is.EqualTo(@"<a href=""""><img alt=""alt"" src=""test://image"" /></a>")); // Route Data not returned
+        }
+
+        [Test]
+        public void ImageActionLinkTest()
+        {
+            var target = CreateAjaxHelper(new ViewDataDictionary());
+            var res = target.ImageActionLink("test://image", "alt", "Action", null, new System.Web.Mvc.Ajax.AjaxOptions { UpdateTargetId="Target" });
+            Assert.That(res, Is.EqualTo(@"<a href="""" onclick=""Sys.Mvc.AsyncHyperlink.handleClick(this, new Sys.UI.DomEvent(event), { insertionMode: Sys.Mvc.InsertionMode.replace, updateTargetId: &#39;Target&#39; });""><img alt=""alt"" src=""test://image"" /></a>")); // Route Data not returned
+        }
+
+        public static HtmlHelper CreateHtmlHelper(ViewDataDictionary vd)
+        {
+            var rd = new RouteData();
+            rd.Route = new Route("controller/action", new MvcRouteHandler());
+            rd.RouteHandler = new MvcRouteHandler();
+            var mockViewContext = new Mock<ViewContext>(
+              new ControllerContext(
+                Mock.Of<HttpContextBase>(),
+                rd,
+                Mock.Of<ControllerBase>()),
+              Mock.Of<IView>(),
+              vd,
+              new TempDataDictionary(),
+              Mock.Of<System.IO.TextWriter>());
+
+            var mockViewDataContainer = new Mock<IViewDataContainer>();
+            mockViewDataContainer.Setup(v => v.ViewData).Returns(vd);
+
+            return new HtmlHelper(mockViewContext.Object, mockViewDataContainer.Object);
+        }
+
+        public static AjaxHelper CreateAjaxHelper(ViewDataDictionary vd)
+        {
+            var rd = new RouteData();
+            rd.Route = new Route("controller/action", new MvcRouteHandler());
+            rd.RouteHandler = new MvcRouteHandler();
+            var mockViewContext = new Mock<ViewContext>(
+              new ControllerContext(
+                Mock.Of<HttpContextBase>(),
+                rd,
+                Mock.Of<ControllerBase>()),
+              Mock.Of<IView>(),
+              vd,
+              new TempDataDictionary(),
+              Mock.Of<System.IO.TextWriter>());
+
+            var mockViewDataContainer = new Mock<IViewDataContainer>();
+            mockViewDataContainer.Setup(v => v.ViewData).Returns(vd);
+
+            return new AjaxHelper(mockViewContext.Object, mockViewDataContainer.Object);
+        }
+    }
+
+
+}
---
+++ Athena.Mvc/ImageLinkExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Mvc.Ajax;
+
+namespace System.Web.Mvc
+{
+    public static class ImageLinkExtensions
+    {
+
+        /// <summary>
+        /// A Simple ActionLink Image
+        /// </summary>
+        /// <param name="actionName">name of the action in controller</param>
+        /// <param name="imgUrl">url of the image</param>
+        /// <param name="alt">alt text of the image</param>
+        /// <returns></returns>
+        public static string ImageLink(this HtmlHelper helper, string actionName, string imgUrl, string alt)
+        {
+            return ImageLink(helper, actionName, imgUrl, alt, null, null, null);
+        }
+
+        public static string ImageActionLink(this AjaxHelper helper, string imageUrl, string altText, string actionName, object routeValues, AjaxOptions ajaxOptions)
+        {
+            var builder = new TagBuilder("img");
+            builder.MergeAttribute("src", imageUrl);
+            builder.MergeAttribute("alt", altText);
+            var link = helper.ActionLink("[replaceme]", actionName, routeValues, ajaxOptions);
+            return link.ToString().Replace("[replaceme]", builder.ToString(TagRenderMode.SelfClosing));
+        }
+
+        /// <summary>
+        /// A Simple ActionLink Image
+        /// </summary>
+        /// <param name="actionName">name of the action in controller</param>
+        /// <param name="imgUrl">url of the iamge</param>
+        /// <param name="alt">alt text of the image</param>
+        /// <returns></returns>
+        public static string ImageLink(this HtmlHelper helper, string actionName, string imgUrl, string alt, object routeValues)
+        {
+            return ImageLink(helper, actionName, imgUrl, alt, routeValues, null, null);
+        }
+
+        /// <summary>
+        /// A Simple ActionLink Image
+        /// </summary>
+        /// <param name="actioNName">name of the action in controller</param>
+        /// <param name="imgUrl">url of the image</param>
+        /// <param name="alt">alt text of the image</param>
+        /// <param name="linkHtmlAttributes">attributes for the link</param>
+        /// <param name="imageHtmlAttributes">attributes for the image</param>
+        /// <returns></returns>
+        public static string ImageLink(this HtmlHelper helper, string actionName, string imgUrl, string alt, object routeValues, object linkHtmlAttributes, object imageHtmlAttributes)
+        {
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            var url = urlHelper.Action(actionName, routeValues);
+
+            //Create the link
+            var linkTagBuilder = new TagBuilder("a");
+            linkTagBuilder.MergeAttribute("href", url);
+            linkTagBuilder.MergeAttributes(new RouteValueDictionary(linkHtmlAttributes));
+
+            //Create image
+            var imageTagBuilder = new TagBuilder("img");
+            imageTagBuilder.MergeAttribute("src", urlHelper.Content(imgUrl));
+            imageTagBuilder.MergeAttribute("alt", urlHelper.Content(alt));
+            imageTagBuilder.MergeAttributes(new RouteValueDictionary(imageHtmlAttributes));
+
+            //Add image to link
+            linkTagBuilder.InnerHtml = imageTagBuilder.ToString(TagRenderMode.SelfClosing);
+
+            return linkTagBuilder.ToString();
+        }
+    }
+}
